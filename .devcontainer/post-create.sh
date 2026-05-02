3d7167af584d689ab80f9fe9541e7c4a8b4a861e
6ameDev <sumitdas.usit@gmail.com>
2026-05-03 04:04:19 +0530

    Removed Dockerfile. Switched to language based devcontainer images

--- .devcontainer/post-create.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-mkdir -p /home/vscode/.config /home/vscode/.local /home/vscode/.cache
-chown -R vscode:vscode /home/vscode/.config /home/vscode/.local /home/vscode/.cache
